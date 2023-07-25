cc4fd00c6743c7b7ed06b60acba43b4dce4d8629
gangmin <minheek99@gmail.com>
2023-07-26 02:12:01 +0900

    Time: 66 ms (24.33%), Space: 41.6 MB (91.37%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -6,11 +6,29 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-    const len = m + n;
-    let j = 0;
-    for(let i = m; i < len; i++) {
-        nums1[i] = nums2[j];
-        j++;
+    // Initialize i and j to store indices of the last element of 1st and 2nd array respectively...
+    let i = m - 1 , j = n - 1;
+    // Initialize a variable k to store the last index of the 1st array...
+    let k = m + n - 1;
+    // Create a loop until either of i or j becomes zero...
+    while(i >= 0 && j >= 0) {
+        if(nums1[i] >= nums2[j]) {
+            nums1[k] = nums1[i];
+            i--;
+        } else {
+            nums1[k] = nums2[j];
+            j--;
+        }
+        k--;
+    // Either of i or j is not zero, which means some elements are yet to be merged.
+    // Being already in a sorted manner, append them to the 1st array in the front.
     }
-    nums1.sort((a, b) => a - b);
+    // While i does not become zero...
+    while(i >= 0)
+        nums1[k--] = nums1[i--];
+    // While j does not become zero...
+    while(j >= 0)
+        nums1[k--] = nums2[j--];
+    // Now 1st array has all the elements in the required sorted order...
+    return;
 };
