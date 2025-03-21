f1d86b97c7c38aaccef81c44303cdc3ae6aab2ff
KANG MIN <minheek99@gmail.com>
2025-03-21 22:28:06 +0900

    Time: 926 ms (5.09%), Space: 68.1 MB (33.9%) - LeetHub

--- 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.js
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.js
@@ -10,16 +10,13 @@ var findAllRecipes = function (recipes, ingredients, supplies) {
         map.set(supplies[i], 1)
     }
 
-    // 재료를 돌면서 map에 추가하는데.. 재료로 요리를 쓰는 요리 또한 존재한다.
-    // 이걸 계속 반복문을 돌아야하는가??
-
     let n = 100;
     while (n > 1) {
         for (let i = 0; i < ingredients.length; i++) {
             const canMake = ingredients[i].every((item) => {
                 return map.get(item) > 0
             })
-            // console.log(canMake)
+
             if (canMake) {
                 map.set(recipes[i], 1)
                 supplies.push(recipes[i])
@@ -28,13 +25,12 @@ var findAllRecipes = function (recipes, ingredients, supplies) {
         n--;
     }
 
-    // console.log(map)
     let res = []
     for (let i = 0; i < ingredients.length; i++) {
         const canMake = ingredients[i].every((item) => {
             return map.get(item) > 0
         })
-        // console.log(canMake)
+
         if (canMake) {
             res.push(recipes[i])
         }
@@ -42,3 +38,6 @@ var findAllRecipes = function (recipes, ingredients, supplies) {
 
     return res;
 };
+
+// 재료를 돌면서 map에 추가하는데.. 재료로 요리를 쓰는 요리 또한 존재한다.
+// 이걸 계속 반복문을 돌아야하는가??
