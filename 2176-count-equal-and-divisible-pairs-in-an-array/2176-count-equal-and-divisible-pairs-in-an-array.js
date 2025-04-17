6cf020b140bf7745e87b35c4a20c7a0d50c4fca4
KANG MIN <minheek99@gmail.com>
2025-04-17 22:14:51 +0900

    Time: 7 ms (5.68%), Space: 56 MB (7.86%) - LeetHub

--- 2176-count-equal-and-divisible-pairs-in-an-array/2176-count-equal-and-divisible-pairs-in-an-array.js
+++ 2176-count-equal-and-divisible-pairs-in-an-array/2176-count-equal-and-divisible-pairs-in-an-array.js
@@ -4,28 +4,47 @@
  * @return {number}
  */
 var countPairs = function (nums, k) {
-    const map = new Map()
-    for (let i = 0; i < nums.length; i++) {
-        const val = map.get(nums[i])
-        if (val) {
-            val.push(i)
-            map.set(nums[i], val)
-        } else {
-            map.set(nums[i], [i])
-        }
-    }
+    // const map = new Map()
+    // for (let i = 0; i < nums.length; i++) {
+    //     const val = map.get(nums[i])
+    //     if (val) {
+    //         val.push(i)
+    //         map.set(nums[i], val)
+    //     } else {
+    //         map.set(nums[i], [i])
+    //     }
+    // }
 
-    let cnt = 0;
-    for (let [key, value] of map) {
-        if (value.length >= 2) {
-            for (let i = 0; i < value.length; i++) {
-                for (let j = i + 1; j < value.length; j++) {
-                    if (value[i] * value[j] % k === 0) {
-                        cnt++;
-                    }
-                }
+    // let cnt = 0;
+    // for (let [key, value] of map) {
+    //     if (value.length >= 2) {
+    //         for (let i = 0; i < value.length; i++) {
+    //             for (let j = i + 1; j < value.length; j++) {
+    //                 if (value[i] * value[j] % k === 0) {
+    //                     cnt++;
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
+    // return cnt;
+    let pos = {};
+    let count = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (!pos[nums[i]]) {
+            pos[nums[i]] = [];
+        }
+
+        for (let j of pos[nums[i]]) {
+            if ((i * j) % k === 0) {
+                count++;
             }
         }
+
+        pos[nums[i]].push(i);
     }
-    return cnt;
+
+    return count;
 };
+
