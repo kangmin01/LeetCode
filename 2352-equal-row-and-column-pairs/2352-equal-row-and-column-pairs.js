d6e6a9d70cc7561dea21db388c86a9458dec6bc8
KANG MIN <minheek99@gmail.com>
2025-07-13 21:36:23 +0900

    Time: 24 ms (96.24%), Space: 62.7 MB (91.68%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.js
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.js
@@ -12,13 +12,32 @@ var equalPairs = function (grid) {
     // }
 
     // // columns 배열 추출?해서 비교하려고 했는데 시간초과.
-    const rows = grid.map(arr => arr.join())
-    const columns = grid[0].map((col, i) => grid.map(row => row[i]).join())
+
+    // (1)
+    // const rows = grid.map(arr => arr.join())
+    // const columns = grid[0].map((col, i) => grid.map(row => row[i]).join())
+    // let count = 0
+    // for (let row of rows) {
+    //     for (let column of columns) {
+    //         if (row === column) count++
+    //     }
+    // }
+    // return count
+
+    // (2)
+      const n = grid.length
     let count = 0
-    for (let row of rows) {
-        for (let column of columns) {
-            if (row === column) count++
-        }
+    const rows = new Map()
+
+    for(let r = 0; r < n; r++){
+        const row = JSON.stringify(grid[r])
+        rows.set(row, 1 + (rows.get(row) || 0))
     }
+
+    for(let c = 0; c < n; c++){
+        const col = JSON.stringify(grid.map(row => row[c]))
+        count += (rows.get(col) || 0)
+    }
+    
     return count
 };
